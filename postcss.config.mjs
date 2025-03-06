6ab358174537ce06256c51790f1598d0e4fb005f
Brad Bertsche <brad@rageix.com>
2025-03-05 19:05:46 -0600

    * updates

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,12 +1,6 @@
 const config = {
   plugins: {
-    '@tailwindcss/postcss': {
-      // might have to take this out one day
-      // project builds fine but
-      // currently breaks vercel --prod
-      // https://github.com/vercel/next.js/issues/75817
-      optimize: { minify: false },
-    },
+    "@tailwindcss/postcss": {},
   },
 };
 export default config;
